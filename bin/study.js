9aaa1b74903a23875c3b56c0f5f29dc76f4b6d96
kiraamaa <kira.a.maclean@gmail.com>
2016-11-07 19:07:51 -0500

    Complete ruby-vs-js-arrays study
    Used Google and class resources to complete

--- bin/study.js
+++ bin/study.js
@@ -1,5 +1,22 @@
+'use strict';
+
 // Repeat Ruby Lab for JS
-// let myArray = [1, 2, 3, 4, 5];
+
+let array = [1, 2, 3, 4, 5]; // undefined
+
+array[array.length + 1] = 20; // 20
+
+array[array.length - 3]; // 5
+
+array[9]; // undefined
+
+array.splice(array.length-5, 3, [-12, -49]); // [ 1, 2, [ -12, -49 ], , 20 ]
+
+for (let i = 1; i < array.length; i++) { // 2 -12 -49 undefined 20
+  return array[i]
+};
 
 
-// Repeat Ruby Bonus for JS
+
+
+let arrayTimesTwo = new Array(10).fill(1).map((e, i) => i *2);
