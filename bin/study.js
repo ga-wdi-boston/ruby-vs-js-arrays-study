c6d6632afd5bff298723885fe98510a86b20cbd3
rickdean <rickdean@outlook.com>
2016-11-07 20:24:00 -0500

    Commit Responses to Studies Study responses

--- bin/study.js
+++ bin/study.js
@@ -1,5 +1,14 @@
+'use strict';
 // Repeat Ruby Lab for JS
 // let myArray = [1, 2, 3, 4, 5];
-
+let myArray = [1, 2, 3, 4, 5];
+myArray[myArray.length+1] = 20;
+myArray[myArray.length-3];
+myArray[9];
+myArray.splice(myArray.length-5, 3, [-12, -29]);
+myArray = [].concat.apply([], myArray);
+for (let i = 1; i < myArray.length; i++) {
+  console.log(myArray[i]);
+}
 
 // Repeat Ruby Bonus for JS
