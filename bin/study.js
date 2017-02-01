e6b589acc940e3d06e0c6cbeacc0d1e750ff7280
mario7746 <m.kennedykavouras@gmail.com>
2017-02-01 08:50:42 -0500

    ruby-vs-js-array-study response 2.1.2017

--- bin/study.js
+++ bin/study.js
@@ -1,5 +1,20 @@
 // Repeat Ruby Lab for JS
-// let myArray = [1, 2, 3, 4, 5];
+my_array = [1, 2, 3, 4, 5]
 
-
+-   Assign `20` to the element at `length+1`.
+-   Access the 3rd element from the end of an array length of 5.
+-   Access element 9 for a length of 5 elements.
+-   Assign [-12, -49] to the range starting from 5th element from the end,
+    for a length of 3.
+-   Access all the elements starting at index 1.
 // Repeat Ruby Bonus for JS
+
+my_array[my_array.length] = 20
+
+my_array (-3, 1)
+
+my array (8, 5)
+
+my_array[-5] = -12, -49
+
+my_array (1, 5)
